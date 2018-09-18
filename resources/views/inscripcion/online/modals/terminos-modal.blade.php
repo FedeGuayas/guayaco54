983427119150f3904a592b65d50e205cac22f875
Hector Alvarez <halain80@gmail.com>
2018-09-18 11:51:18 -0500

    agregado los terminos y condiciones

--- resources/views/inscripcion/online/modals/terminos-modal.blade.php
+++ resources/views/inscripcion/online/modals/terminos-modal.blade.php
@@ -1,23 +1,82 @@
-<div class="modal fade bs-example-modal-lg" id="terminos-modal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
-    <div class="modal-dialog modal-lg modal-dialog-centered">
+<div class="modal fade" id="terminos-modal" tabindex="-1" role="dialog" aria-labelledby="termModalLabel"
+     aria-hidden="true">
+    <div class="modal-dialog modal-lg" role="document" style=" width: 70% !important;">
         <div class="modal-content">
             <div class="modal-header">
-                <h4 class="modal-title" id="myLargeModalLabel">Términos y Condiciones</h4>
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
+                <h5 class="modal-title text-red-50" id="termModalLabel">Condiciones de inscripción online a la Carrera
+                    Guayaco
+                    Runner</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Cerrar">
+                    <span aria-hidden="true">&times;</span>
+                </button>
             </div>
-            <div class="modal-body">
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+            <div class="modal-body" style="font-size: 12px; font-weight: 500;">
+
+                <h5 class="text-blue-50">Términos y condiciones de inscripción a la Carrera Guayaco Runner V edición
+                    (2018) </h5>
+                <p>
+
+                <dl>
+                    <li>
+                        Este sistema es exclusivamente para inscripción y pago del registro en la carrera Guayaco Runner
+                        V edición.
+                    </li>
+                    <li>
+                        El kit incluye: camiseta, bolso y medalla de participación.
+                    </li>
+                    <li>
+                        Los kits sólo pueden ser retirados el día sábado 27 de octubre a partir de las 09h00 am hasta
+                        15h00 en el Coliseo de Vóley en la Explanada del Estadio Modelo (Av. De las Américas y Av.
+                        Kennedy) presentando la Cédula de identidad y el ticket de inscripción.
+                    </li>
+                    <li>
+                        En caso de requerir cambio de talla, puede hacerlo en la mesa de información.
+                    </li>
+                    <li>
+                        Todo ticket que no se encuentre en estado PAGADO no tendrá derecho a reclamación del kit, el
+                        usuario puede cancelar en efectivo su inscripción hasta el 27 de octubre del 2018.
+                    </li>
+                    <li>
+                        Los participantes están sujetos al Reglamento de la carrera publicado en la Página web
+                        institucional. <a href="https://fedeguayas.com.ec" target="_blank">www.fedeguayas.com.ec</a>
+                    </li>
+                    <li>
+                        Las personas con discapacidad y/ó tercera edad que deseen acceder al 50% de descuento, deberán
+                        acercarse a realizar su inscripción de manera presencial portando su cédula de identidad o
+                        carnet del Conadis.
+                    </li>
+                    <li>
+                        La factura electrónica llegará 24 horas después de realizado el pago al correo electrónico
+                        especificado en los datos de facturación.
+                    </li>
+                </dl>
+                </p>
+                <h5 class="text-blue-50">Medios de pago</h5>
+                <p>
+                <dl>
+                    <li>
+                        El sistema acepta como medios de pago tarjetas de crédito: Diners Club, Discover, Visa,
+                        Mastercard,
+                        American Express, Maestro y Electrón de débito y crédito como MasterCard, Visa, Electrón.
+                    </li>
+                    <li>
+                        El uso, condiciones de pago y otras condiciones aplicables a las tarjetas de crédito, son de
+                        exclusiva responsabilidad de su emisor.
+                    </li>
+                </dl>
+                </p>
+                <h5 class="text-blue-50">Derecho de retracto y devoluciones</h5>
+                <p>
+                <dl>
+                    <li>
+                        No aplica derecho de Retracto.
+                    </li>
+                    <li>
+                        Una vez emitida la factura no se aceptan devoluciones.
+                    </li>
+                </dl>
+                </p>
+
             </div>
             <div class="modal-footer">
                 <button type="button" class="btn btn-primary" data-dismiss="modal">Aceptar</button>
